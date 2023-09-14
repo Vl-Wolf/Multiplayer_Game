8e8e99af29056fafad7564c8dd51abad509cf6f5
Vladislav <vvd6565@gmail.com>
2023-09-14 12:41:34 +0300

    Initial commit

---
+++ Source/TT/TTGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TTGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ATTGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ATTGameMode();
+};
+
+
+
